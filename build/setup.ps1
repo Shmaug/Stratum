49befdf736267763a648011a39372e980606bddd
Trevor Hedstrom <Shmaug@users.noreply.github.com>
2020-09-05 20:08:13 -0700

    streamlined deps

--- build/setup.ps1
+++ build/setup.ps1
@@ -1,54 +1,27 @@
-param (
-  [String]$BuildType="RelWithDebInfo", 
-  [Boolean]$ConfigureDeps=$true, 
-  [Boolean]$BuildDeps=$false
-)
+$STRATUM_DIR = (Resolve-Path (Join-Path $PSScriptRoot /..)).Path
 
-$STRATUM_DIR     = (Resolve-Path (Join-Path $PSScriptRoot /..)).Path
-$ASSIMP_DIR      = "$STRATUM_DIR/ThirdParty/src/assimp"
-$SHADERC_DIR     = "$STRATUM_DIR/ThirdParty/src/shaderc"
-$MSDFGEN_DIR     = "$STRATUM_DIR/ThirdParty/src/msdfgen"
-$MSDFGEN_DIR     = "$STRATUM_DIR/ThirdParty/src/msdfgen"
-$SPIRV_CROSS_DIR = "$STRATUM_DIR/ThirdParty/src/SPIRV-Cross"
-$OPENXR_DIR      = "$STRATUM_DIR/ThirdParty/src/OpenXR-SDK"
-
-function ConfigureTarget ($dir, $configure_args) {
-  $command = "cmake -S $dir -B $dir/build"
-  $command = $command + " -DCMAKE_BUILD_TYPE="+$BuildType+" -DCMAKE_INSTALL_PREFIX="+$STRATUM_DIR+"/ThirdParty -Wno-dev " + $configure_args
+function BuildTarget ($FolderName, $ConfigureArguments) {
+  $command = "cmake -S $STRATUM_DIR/ThirdParty/src/$FolderName -B $STRATUM_DIR/ThirdParty/src/$FolderName/build"
+  $command = $command + " -G Ninja"
+  $command = $command + " -DCMAKE_CONFIGURATION_TYPES='Debug;Release' -DCMAKE_INSTALL_PREFIX="+$STRATUM_DIR+"/ThirdParty -Wno-dev " + $ConfigureArguments
   Invoke-Expression $command
+  cmake --build "${STRATUM_DIR}/ThirdParty/src/$FolderName/build" --target install
 }
 
-Invoke-Webrequest https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp -Outfile ThirdParty/include/json.hpp
-Invoke-Webrequest https://raw.githubusercontent.com/nothings/stb/master/stb_image.h -Outfile ThirdParty/include/stb_image.h
-Invoke-Webrequest https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h -Outfile ThirdParty/include/stb_image_write.h
-Invoke-Webrequest https://raw.githubusercontent.com/syoyo/tinygltf/master/tiny_gltf.h -Outfile ThirdParty/include/tiny_gltf.h
+Remove-Item -LiteralPath $STRATUM_DIR/ThirdParty/ -Force -Recurse
+mkdir -Force ThirdParty/include
+
+Invoke-Webrequest -Outfile ThirdParty/include/json.hpp          -Uri "https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp"
+Invoke-Webrequest -Outfile ThirdParty/include/stb_image.h       -Uri "https://raw.githubusercontent.com/nothings/stb/master/stb_image.h"
+Invoke-Webrequest -Outfile ThirdParty/include/stb_image_write.h -Uri "https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h"
+Invoke-Webrequest -Outfile ThirdParty/include/tiny_gltf.h       -Uri "https://raw.githubusercontent.com/syoyo/tinygltf/master/tiny_gltf.h"
 
 git submodule update --init
-Set-Location $SHADERC_DIR
-python utils/git-sync-deps
-Set-Location $STRATUM_DIR
+python ./ThirdParty/src/shaderc/utils/git-sync-deps
 
-
-if ($ConfigureDeps) {
-  Write-Host "Configuring dependencies [$BuildType]"
-  Write-Host ""
-
-  ConfigureTarget $ASSIMP_DIR '-DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ZLIB=ON'
-  ConfigureTarget $MSDFGEN_DIR '-DMSDFGEN_BUILD_MSDFGEN_STANDALONE=OFF -DMSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>DLL -DFREETYPE_INCLUDE_DIRS="$MSDFGEN_DIR/freetype/include" -DFREETYPE_LIBRARY="$MSDFGEN_DIR/freetype/win64/freetype.lib" '
-  ConfigureTarget $OPENXR_DIR '-DDYNAMIC_LOADER=ON'
-  ConfigureTarget $SHADERC_DIR '-DSHADERC_ENABLE_SHARED_CRT=ON -DBUILD_SHARED_LIBS=OFF -DSHADERC_SKIP_TESTS=ON -DBUILD_TESTING=OFF -DSPIRV_SKIP_EXECUTABLES=ON'
-  ConfigureTarget $SPIRV_CROSS_DIR '-DSPIRV_CROSS_ENABLE_TESTS=OFF'
-}
-
-if ($BuildDeps) {
-  Write-Host "Building dependencies"
-  Write-Host ""
-
-  cmake --build . --config $BuildType --target add-copyright
-
-  cmake --build "$ASSIMP_DIR/build"      --config $BuildType --target install
-  cmake --build "$MSDFGEN_DIR/build"     --config $BuildType --target install
-  cmake --build "$OPENXR_DIR/build"      --config $BuildType --target install
-  cmake --build "$SHADERC_DIR/build"     --config $BuildType --target install
-  cmake --build "$SPIRV_CROSS_DIR/build" --config $BuildType --target install
-}
+BuildTarget assimp      '-DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ZLIB=ON'
+BuildTarget freetype2   '-DMSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>DLL'
+BuildTarget msdfgen     '-DMSDFGEN_BUILD_MSDFGEN_STANDALONE=OFF -DMSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>DLL -DFREETYPE_INCLUDE_DIRS="${STRATUM_DIR}/ThirdParty/include" -DFREETYPE_LIBRARY="${STRATUM_DIR}/ThirdParty/lib/freetype.lib"'
+BuildTarget OpenXR-SDK  '-DDYNAMIC_LOADER=ON'
+BuildTarget shaderc     '-DSHADERC_ENABLE_SHARED_CRT=ON -DBUILD_SHARED_LIBS=OFF -DSHADERC_SKIP_TESTS=ON -DBUILD_TESTING=OFF -DSPIRV_SKIP_EXECUTABLES=ON'
+BuildTarget SPIRV-Cross '-DSPIRV_CROSS_ENABLE_TESTS=OFF'
--- build/stratum.cmake
+++ build/stratum.cmake
@@ -1,66 +1,22 @@
 set(CMAKE_CXX_STANDARD 17)
 
-file(GLOB_RECURSE THIRD_PARTY_CONFIGS  "${STRATUM_HOME}/ThirdParty/**Config.cmake")
-foreach(f ${THIRD_PARTY_CONFIGS})
-	get_filename_component(dir ${f} DIRECTORY)
-	string(FIND ${dir} "${STRATUM_HOME}/ThirdParty/src" idx)
-	if (${idx} EQUAL -1)
-		list(APPEND CMAKE_PREFIX_PATH ${dir})
-	endif()
-endforeach()
+function(stm_compile_definitions TARGET_NAME)
+	target_include_directories(${TARGET_NAME} PUBLIC "${STRATUM_HOME}")
 
-function(link_plugin TARGET_NAME)
-	add_dependencies(${TARGET_NAME} Stratum)
-	target_link_libraries(${TARGET_NAME} "${PROJECT_BINARY_DIR}/lib/Stratum.lib")
-
-	target_compile_definitions(${TARGET_NAME} PUBLIC XR_USE_GRAPHICS_API_VULKAN)
+	target_compile_definitions(${TARGET_NAME} STRATUM_VERSION_MAJOR=1 STRATUM_VERSION_MINOR=1 PUBLIC XR_USE_GRAPHICS_API_VULKAN)
 	if (${ENABLE_DEBUG_LAYERS})
 		target_compile_definitions(${TARGET_NAME} PUBLIC ENABLE_DEBUG_LAYERS)
 	endif()
 	
-	target_include_directories(${TARGET_NAME} PUBLIC "${STRATUM_HOME}" "${STRATUM_HOME}/ThirdParty/include" "$ENV{VULKAN_SDK}/include")
-	target_link_directories(${TARGET_NAME} PUBLIC "${STRATUM_HOME}/ThirdParty/lib")
-
 	if (WIN32)
-		if(DEFINED ENV{VULKAN_SDK})
-			message(STATUS "Found VULKAN_SDK: $ENV{VULKAN_SDK}")
-		else()
-			message(FATAL_ERROR "Error: VULKAN_SDK not set!")
-		endif()
-
-		target_link_directories(${TARGET_NAME} PUBLIC "${STRATUM_HOME}/ThirdParty/msdfgen/freetype/win64")
-
 		target_compile_definitions(${TARGET_NAME} PUBLIC VK_USE_PLATFORM_WIN32_KHR WINDOWS WIN32_LEAN_AND_MEAN NOMINMAX _CRT_SECURE_NO_WARNINGS)
 		if (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
 			target_compile_definitions(${TARGET_NAME} PUBLIC _CRTDBG_MAP_ALLOC)
 		endif()
-		
-		target_link_libraries(${TARGET_NAME} "Ws2_32.lib" "$ENV{VULKAN_SDK}/lib/vulkan-1.lib" )
-		if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-			target_link_libraries(${TARGET_NAME} "assimpd.lib" "zlibstaticd.lib" "IrrXMLd.lib")
-		else()
-			target_link_libraries(${TARGET_NAME} "assimp.lib" "zlibstatic.lib" "IrrXML.lib")
-		endif()
-
-		if (${ENABLE_DEBUG_LAYERS})
-			target_link_libraries(${TARGET_NAME} "$ENV{VULKAN_SDK}/lib/VkLayer_utils.lib")
-		endif()
 	endif(WIN32)
-
-	set_target_properties(${TARGET_NAME} PROPERTIES
-		RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/bin/Plugins"
-		LIBRARY_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/bin/Plugins"
-		ARCHIVE_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/lib/Plugins"
-		RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO "${CMAKE_BINARY_DIR}/bin/Plugins"
-		LIBRARY_OUTPUT_DIRECTORY_RELWITHDEBINFO "${CMAKE_BINARY_DIR}/bin/Plugins"
-		ARCHIVE_OUTPUT_DIRECTORY_RELWITHDEBINFO "${CMAKE_BINARY_DIR}/lib/Plugins"
-		RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/bin/Plugins"
-		LIBRARY_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/bin/Plugins"
-		ARCHIVE_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/lib/Plugins")
-
 endfunction()
 
-function(add_shader_target TARGET_NAME FOLDER_PATH)
+function(stm_shader_target TARGET_NAME FOLDER_PATH)
 	# Compile shaders in Shaders/* using ShaderCompiler
 	file(GLOB_RECURSE SHADER_SOURCES
 		"${FOLDER_PATH}*.frag"
