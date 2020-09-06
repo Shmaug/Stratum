51922581c10bc57d4fbeeab6a5b922e95b45e3ce
Trevor Hedstrom <Shmaug@users.noreply.github.com>
2020-09-05 18:24:46 -0700

    dependencies again

--- build/setup.ps1
+++ build/setup.ps1
@@ -1,37 +1,54 @@
-param ([String] $buildType='Release',  $generator='Ninja')
+param (
+  [String]$BuildType="RelWithDebInfo", 
+  [Boolean]$ConfigureDeps=$true, 
+  [Boolean]$BuildDeps=$false
+)
 
+$STRATUM_DIR     = (Resolve-Path (Join-Path $PSScriptRoot /..)).Path
+$ASSIMP_DIR      = "$STRATUM_DIR/ThirdParty/src/assimp"
+$SHADERC_DIR     = "$STRATUM_DIR/ThirdParty/src/shaderc"
+$MSDFGEN_DIR     = "$STRATUM_DIR/ThirdParty/src/msdfgen"
+$MSDFGEN_DIR     = "$STRATUM_DIR/ThirdParty/src/msdfgen"
+$SPIRV_CROSS_DIR = "$STRATUM_DIR/ThirdParty/src/SPIRV-Cross"
+$OPENXR_DIR      = "$STRATUM_DIR/ThirdParty/src/OpenXR-SDK"
 
-Set-Variable -Name STRATUM_DIR      -Value (Resolve-Path (Join-Path $PSScriptRoot /..)).Path
-Set-Variable -Name ASSIMP_DIR       -Value "$STRATUM_DIR/ThirdParty/repos/assimp"
-Set-Variable -Name SHADERC_DIR      -Value "$STRATUM_DIR/ThirdParty/repos/shaderc"
-Set-Variable -Name MSDFGEN_DIR      -Value "$STRATUM_DIR/ThirdParty/repos/msdfgen"
-Set-Variable -Name MSDFGEN_DIR      -Value "$STRATUM_DIR/ThirdParty/repos/msdfgen"
-Set-Variable -Name SPIRV_CROSS_DIR  -Value "$STRATUM_DIR/ThirdParty/repos/SPIRV-Cross"
-Set-Variable -Name OPENXR_DIR       -Value "$STRATUM_DIR/ThirdParty/repos/OpenXR-SDK"
+function ConfigureTarget ($dir, $configure_args) {
+  $command = "cmake -S $dir -B $dir/build"
+  $command = $command + " -DCMAKE_BUILD_TYPE="+$BuildType+" -DCMAKE_INSTALL_PREFIX="+$STRATUM_DIR+"/ThirdParty -Wno-dev " + $configure_args
+  Invoke-Expression $command
+}
+
+Invoke-Webrequest https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp -Outfile ThirdParty/include/json.hpp
+Invoke-Webrequest https://raw.githubusercontent.com/nothings/stb/master/stb_image.h -Outfile ThirdParty/include/stb_image.h
+Invoke-Webrequest https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h -Outfile ThirdParty/include/stb_image_write.h
+Invoke-Webrequest https://raw.githubusercontent.com/syoyo/tinygltf/master/tiny_gltf.h -Outfile ThirdParty/include/tiny_gltf.h
 
 git submodule update --init
-
-Set-Location "$SHADERC_DIR"
+Set-Location $SHADERC_DIR
 python utils/git-sync-deps
+Set-Location $STRATUM_DIR
 
-Set-Location "$STRATUM_DIR"
 
-Write-Host "Configuring dependencies [$buildType] [$generator]"
-Write-Host ""
+if ($ConfigureDeps) {
+  Write-Host "Configuring dependencies [$BuildType]"
+  Write-Host ""
 
-cmake -S "$ASSIMP_DIR"      -B "$ASSIMP_DIR/build"      -G $generator -DCMAKE_BUILD_TYPE="$buildType" -DCMAKE_INSTALL_PREFIX="$STRATUM_DIR/ThirdParty" -Wno-dev -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ZLIB=ON -DINJECT_DEBUG_POSTFIX=OFF -DLIBRARY_SUFFIX=""
-cmake -S "$MSDFGEN_DIR"     -B "$MSDFGEN_DIR/build"     -G $generator -DCMAKE_BUILD_TYPE="$buildType" -DCMAKE_INSTALL_PREFIX="$STRATUM_DIR/ThirdParty" -Wno-dev -DMSDFGEN_BUILD_MSDFGEN_STANDALONE=OFF -DMSVC_RUNTIME_LIBRARY="MultiThreaded$<$<CONFIG:Debug>:Debug>DLL" -DFREETYPE_INCLUDE_DIRS="$MSDFGEN_DIR/freetype/include" -DFREETYPE_LIBRARY="$MSDFGEN_DIR/freetype/win64/freetype.lib"
-cmake -S "$OPENXR_DIR"      -B "$OPENXR_DIR/build"      -G $generator -DCMAKE_BUILD_TYPE="$buildType" -DCMAKE_INSTALL_PREFIX="$STRATUM_DIR/ThirdParty" -Wno-dev -DDYNAMIC_LOADER=ON
-cmake -S "$SHADERC_DIR"     -B "$SHADERC_DIR/build"     -G $generator -DCMAKE_BUILD_TYPE="$buildType" -DCMAKE_INSTALL_PREFIX="$STRATUM_DIR/ThirdParty" -Wno-dev -DSHADERC_ENABLE_SHARED_CRT=ON -DBUILD_SHARED_LIBS=OFF -DSHADERC_SKIP_TESTS=ON
-cmake -S "$SPIRV_CROSS_DIR" -B "$SPIRV_CROSS_DIR/build" -G $generator -DCMAKE_BUILD_TYPE="$buildType" -DCMAKE_INSTALL_PREFIX="$STRATUM_DIR/ThirdParty" -Wno-dev -DSPIRV_CROSS_ENABLE_TESTS=OFF
+  ConfigureTarget $ASSIMP_DIR '-DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ZLIB=ON'
+  ConfigureTarget $MSDFGEN_DIR '-DMSDFGEN_BUILD_MSDFGEN_STANDALONE=OFF -DMSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>DLL -DFREETYPE_INCLUDE_DIRS="$MSDFGEN_DIR/freetype/include" -DFREETYPE_LIBRARY="$MSDFGEN_DIR/freetype/win64/freetype.lib" '
+  ConfigureTarget $OPENXR_DIR '-DDYNAMIC_LOADER=ON'
+  ConfigureTarget $SHADERC_DIR '-DSHADERC_ENABLE_SHARED_CRT=ON -DBUILD_SHARED_LIBS=OFF -DSHADERC_SKIP_TESTS=ON -DBUILD_TESTING=OFF -DSPIRV_SKIP_EXECUTABLES=ON'
+  ConfigureTarget $SPIRV_CROSS_DIR '-DSPIRV_CROSS_ENABLE_TESTS=OFF'
+}
 
-Write-Host "Building dependencies"
-Write-Host ""
+if ($BuildDeps) {
+  Write-Host "Building dependencies"
+  Write-Host ""
 
-cmake --build . --config $buildType --target add-copyright
+  cmake --build . --config $BuildType --target add-copyright
 
-cmake --build "$ASSIMP_DIR/build"      --config $buildType --target install
-cmake --build "$MSDFGEN_DIR/build"     --config $buildType --target install
-cmake --build "$OPENXR_DIR/build"      --config $buildType --target install
-cmake --build "$SHADERC_DIR/build"     --config $buildType --target install
-cmake --build "$SPIRV_CROSS_DIR/build" --config $buildType --target install
+  cmake --build "$ASSIMP_DIR/build"      --config $BuildType --target install
+  cmake --build "$MSDFGEN_DIR/build"     --config $BuildType --target install
+  cmake --build "$OPENXR_DIR/build"      --config $BuildType --target install
+  cmake --build "$SHADERC_DIR/build"     --config $BuildType --target install
+  cmake --build "$SPIRV_CROSS_DIR/build" --config $BuildType --target install
+}
--- build/stratum.cmake
+++ build/stratum.cmake
@@ -1,40 +1,51 @@
 set(CMAKE_CXX_STANDARD 17)
 
-add_compile_definitions(XR_USE_GRAPHICS_API_VULKAN)
-if (${ENABLE_DEBUG_LAYERS})
-	add_compile_definitions(ENABLE_DEBUG_LAYERS)
-endif()
-
-include_directories(
-	"${STRATUM_HOME}"
-	"${STRATUM_HOME}/ThirdParty/assimp/include"
-	"${STRATUM_HOME}/ThirdParty/msdfgen/include"
-	"${STRATUM_HOME}/ThirdParty/OpenXR-SDK/include")
-
-if(WIN32)
-	if(DEFINED ENV{VULKAN_SDK})
-		message(STATUS "Found VULKAN_SDK: $ENV{VULKAN_SDK}")
-	else()
-		message(FATAL_ERROR "Error: VULKAN_SDK not set!")
+file(GLOB_RECURSE THIRD_PARTY_CONFIGS  "${STRATUM_HOME}/ThirdParty/**Config.cmake")
+foreach(f ${THIRD_PARTY_CONFIGS})
+	get_filename_component(dir ${f} DIRECTORY)
+	string(FIND ${dir} "${STRATUM_HOME}/ThirdParty/src" idx)
+	if (${idx} EQUAL -1)
+		list(APPEND CMAKE_PREFIX_PATH ${dir})
 	endif()
-
-	add_compile_definitions(VK_USE_PLATFORM_WIN32_KHR)
-	add_compile_definitions(WINDOWS WIN32_LEAN_AND_MEAN NOMINMAX _CRT_SECURE_NO_WARNINGS)
-	if (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
-		add_compile_definitions(_CRTDBG_MAP_ALLOC)
-	endif()
-
-	link_directories("${STRATUM_HOME}/ThirdParty/msdfgen/freetype/win64")
-	include_directories("$ENV{VULKAN_SDK}/include")
-	link_libraries("Ws2_32.lib" "$ENV{VULKAN_SDK}/lib/vulkan-1.lib")
-
-	if (${ENABLE_DEBUG_LAYERS})
-		link_libraries("$ENV{VULKAN_SDK}/lib/VkLayer_utils.lib")
-	endif()
-endif()
+endforeach()
 
 function(link_plugin TARGET_NAME)
 	add_dependencies(${TARGET_NAME} Stratum)
+	target_link_libraries(${TARGET_NAME} "${PROJECT_BINARY_DIR}/lib/Stratum.lib")
+
+	target_compile_definitions(${TARGET_NAME} PUBLIC XR_USE_GRAPHICS_API_VULKAN)
+	if (${ENABLE_DEBUG_LAYERS})
+		target_compile_definitions(${TARGET_NAME} PUBLIC ENABLE_DEBUG_LAYERS)
+	endif()
+	
+	target_include_directories(${TARGET_NAME} PUBLIC "${STRATUM_HOME}" "${STRATUM_HOME}/ThirdParty/include" "$ENV{VULKAN_SDK}/include")
+	target_link_directories(${TARGET_NAME} PUBLIC "${STRATUM_HOME}/ThirdParty/lib")
+
+	if (WIN32)
+		if(DEFINED ENV{VULKAN_SDK})
+			message(STATUS "Found VULKAN_SDK: $ENV{VULKAN_SDK}")
+		else()
+			message(FATAL_ERROR "Error: VULKAN_SDK not set!")
+		endif()
+
+		target_link_directories(${TARGET_NAME} PUBLIC "${STRATUM_HOME}/ThirdParty/msdfgen/freetype/win64")
+
+		target_compile_definitions(${TARGET_NAME} PUBLIC VK_USE_PLATFORM_WIN32_KHR WINDOWS WIN32_LEAN_AND_MEAN NOMINMAX _CRT_SECURE_NO_WARNINGS)
+		if (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+			target_compile_definitions(${TARGET_NAME} PUBLIC _CRTDBG_MAP_ALLOC)
+		endif()
+		
+		target_link_libraries(${TARGET_NAME} "Ws2_32.lib" "$ENV{VULKAN_SDK}/lib/vulkan-1.lib" )
+		if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+			target_link_libraries(${TARGET_NAME} "assimpd.lib" "zlibstaticd.lib" "IrrXMLd.lib")
+		else()
+			target_link_libraries(${TARGET_NAME} "assimp.lib" "zlibstatic.lib" "IrrXML.lib")
+		endif()
+
+		if (${ENABLE_DEBUG_LAYERS})
+			target_link_libraries(${TARGET_NAME} "$ENV{VULKAN_SDK}/lib/VkLayer_utils.lib")
+		endif()
+	endif(WIN32)
 
 	set_target_properties(${TARGET_NAME} PROPERTIES
 		RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/bin/Plugins"
@@ -47,27 +58,6 @@ function(link_plugin TARGET_NAME)
 		LIBRARY_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/bin/Plugins"
 		ARCHIVE_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/lib/Plugins")
 
-	if (WIN32)
-		# Link vulkan and assimp
-		target_link_libraries(${TARGET_NAME}
-				"${PROJECT_BINARY_DIR}/lib/Stratum.lib"
-				"$ENV{VULKAN_SDK}/lib/vulkan-1.lib" )
-		if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-			target_link_libraries(${TARGET_NAME}
-				"${STRATUM_HOME}/ThirdParty/assimp/lib/assimpd.lib"
-				"${STRATUM_HOME}/ThirdParty/assimp/lib/zlibstaticd.lib"
-				"${STRATUM_HOME}/ThirdParty/assimp/lib/IrrXMLd.lib" )
-		else()
-			target_link_libraries(${TARGET_NAME}
-				"${STRATUM_HOME}/ThirdParty/assimp/lib/assimp.lib"
-				"${STRATUM_HOME}/ThirdParty/assimp/lib/zlibstatic.lib"
-				"${STRATUM_HOME}/ThirdParty/assimp/lib/IrrXML.lib" )
-		endif()
-
-		if (${ENABLE_DEBUG_LAYERS})
-			target_link_libraries(${TARGET_NAME} "$ENV{VULKAN_SDK}/lib/VkLayer_utils.lib")
-		endif()
-	endif(WIN32)
 endfunction()
 
 function(add_shader_target TARGET_NAME FOLDER_PATH)
