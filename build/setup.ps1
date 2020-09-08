d15217af579bfb846854afa68abf330a6fbd00c0
Trevor Hedstrom <Shmaug@users.noreply.github.com>
2020-09-07 17:50:08 -0700

    i hate git

--- build/setup.ps1
+++ build/setup.ps1
@@ -1,5 +1,13 @@
 $STRATUM_DIR = (Resolve-Path (Join-Path $PSScriptRoot /..)).Path
 
+Remove-Item -LiteralPath $STRATUM_DIR/extern/ -Force -Recurse
+mkdir -Force extern/include
+mkdir -Force extern/src
+
+git submodule update --init
+
+python ./extern/src/shaderc/utils/git-sync-deps
+
 function BuildTarget ($FolderName, $ConfigureArguments) {
   $command = "cmake -S $STRATUM_DIR/extern/src/$FolderName -B $STRATUM_DIR/extern/src/$FolderName/build"
   $command = $command + " -G Ninja"
@@ -8,11 +16,6 @@ function BuildTarget ($FolderName, $ConfigureArguments) {
   cmake --build "${STRATUM_DIR}/extern/src/$FolderName/build" --config Release --target install
   cmake --build "${STRATUM_DIR}/extern/src/$FolderName/build" --config Debug --target install
 }
-
-Remove-Item -LiteralPath $STRATUM_DIR/extern/ -Force -Recurse
-mkdir -Force extern/include
-
-python ./extern/src/shaderc/utils/git-sync-deps
 
 BuildTarget assimp      '-DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ZLIB=ON'
 BuildTarget freetype2   '-DMSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>DLL'
